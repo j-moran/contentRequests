e1db50d287df52e591f4d2bb7692a3a39cef6394
John J Moran <jfv0827@wade.wmich.edu>
2019-03-31 22:12:01 -0400

    Finish basic setup

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 //=======================
 //General Variables and Requires
 //=======================
-var express 	= require('express')
-	app 		= express(),
-	bodyparser 	= require('body-parser'),
-	mongoose 	= require('mongoose'),
-	flash 		= require('connect-flash');
+var express 		= require('express')
+	app 			= express(),
+	bodyParser 		= require('body-parser'),
+	mongoose 		= require('mongoose'),
+	flash 			= require('connect-flash'),
+	passport 		= require('passport'),
+	LocalStrategy 	= require('passport-local').Strategy,
+	User			= require('./models/user'),
+	Request			= require('./models/request');
 
 //=======================
 //Route Variables
 //=======================
-
+var pageRoutes 		= require('./routes/page'),
+	authRoutes 		= require('./routes/auth');
 
 //=======================
 //Database Setup
 //=======================
+var Database		= require('./scripts/database');
 
 
 //=======================
-//App Setup (Passport and Express)
+//App Setup
 //=======================
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(flash());
+app.use(require('express-session')({
+	secret: 'Maryhadalittlelamb',
+	resave: false,
+	saveUninitialized: false
+}));
 
+app.use(pageRoutes);
+app.use(authRoutes);
 
 //=======================
 //Server Settings
