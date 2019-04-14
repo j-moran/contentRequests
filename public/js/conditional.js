e65e7f5ea8746bbf1422cca3d828f72c46771d77
John J Moran <jfv0827@wade.wmich.edu>
2019-04-14 16:30:21 -0400

    Add form to request page. Needs more work.

--- public/js/conditional.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#ebookRequest').on('click', function () {
-        $('#ebookForm').toggleClass('active');
-    });
-
-    $('button').on('click', function(){
-    	$(this).toggleClass('active');
-    });
-});
