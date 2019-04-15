0410965457b31a174820a03c37577806b629b29c
John Moran <john.moran225@gmail.com>
2019-04-14 23:19:25 -0400

    Change UI and tweak some items

--- routes/page.js
+++ routes/page.js
@@ -1,14 +1,19 @@
 var express 	= require('express'),
 	router		= express.Router(),
 	User		= require('../models/user'),
-	Request		= require('../models/request');
+	Request		= require('../models/request'),
+	api			= new Kitsu();
 
 router.get('/', function(req,res){
 	res.render('landing');
 });
 
-router.get('/profile/:user', function(req,res){
-	res.render('users/index');
+router.get('/profile', function(req,res){
+	res.render('users/index', {currentUser: 'admin'});
+});
+
+router.get('/search', function(req,res){
+
 });
 
 router.get('/request', function(req,res){
