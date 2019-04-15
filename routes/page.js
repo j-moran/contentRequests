22cdbda689ab92e6393637f59b3a166f752cba43
John J Moran <jfv0827@wade.wmich.edu>
2019-04-15 11:07:43 -0400

    Minor changes.

--- routes/page.js
+++ routes/page.js
@@ -1,20 +1,31 @@
 var express 	= require('express'),
 	router		= express.Router(),
 	User		= require('../models/user'),
-	Request		= require('../models/request'),
-	api			= new Kitsu();
+	Request		= require('../models/request');
 
 router.get('/', function(req,res){
 	res.render('landing');
 });
 
 router.get('/profile', function(req,res){
-	res.render('users/index', {currentUser: 'admin'});
+	res.render('users/index');
 });
 
-router.get('/search', function(req,res){
+// router.get('/search', function(req,res){
+// 	switch(req.params.type){
+// 		case 'ebook':
 
-});
+// 			break;
+// 		case 'manga':
+// 			break;
+// 		case 'novel':
+// 			break; 
+// 	};
+// });
+
+//router.get('/results', function(req,res){
+
+//});
 
 router.get('/request', function(req,res){
 	res.render('requests/index');
