e1e040baf125b9d72ea8c89dc2b8caeaa79fc4ed
John Moran <john.moran225@gmail.com>
2019-05-05 21:32:17 -0400

    Create new requests when request link clicked.

--- routes/request.js
+++ routes/request.js
@@ -9,9 +9,32 @@ router.get('/myrequests', middleware.isLoggedIn, function(req,res){
 });
 
 router.post('/myrequests', function(req,res){
-	console.log(req.body);
-	req.flash("success", "Request submitted successfully!");
-	res.redirect('/myrequests');
+	var request = new Request({
+		type: req.body.type,
+		title: req.body.title,
+		id: req.body.id,
+		link: req.protocol + '://' + req.get('host') + req.originalUrl,
+		filled: false,
+		requester: req.body.user
+	});
+
+	request.save(function(err){
+		if(err){
+			console.log(err);
+		};
+	});
+
+	User.findOne({_id: req.body.user}).populate('requests').exec(function(err, requests){
+		if(err){
+			// console.log(err);
+		};
+
+		// console.log(JSON.stringify(requests, null, "\t"));
+
+		req.flash("success", "Request submitted successfully!");
+		res.redirect('/myrequests');
+	});
+
 });
 
 
